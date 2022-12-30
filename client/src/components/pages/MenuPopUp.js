0eee4c3bb12266d4f0a7ee9077ddaad791fb2892
Blake14 <blakemcpherson2@gmail.com>
2022-12-30 17:08:52 -0600

    feat: random game ID generator for hosting

--- client/src/components/pages/MenuPopUp.js
+++ client/src/components/pages/MenuPopUp.js
@@ -3,6 +3,17 @@ import JoinGame from './JoinGame';
 import { FaRegWindowClose } from 'react-icons/fa';
 
 const MenuPopUp = (props) => {
+	const GenerateGameID = (length) => {
+		var length = 5;
+		var result = '';
+		var characters =
+			'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+		var charactersLength = characters.length;
+		for (var i = 0; i < length; i++) {
+			result += characters.charAt(Math.floor(Math.random() * charactersLength));
+		}
+		return result;
+	};
 	if (props.menuType === 'NONE') {
 		return <></>;
 	} else if (props.menuType === 'HOST') {
@@ -11,6 +22,7 @@ const MenuPopUp = (props) => {
 				<HostGame
 					FaRegWindowClose={FaRegWindowClose}
 					setMenuType={props.setMenuType}
+					GenerateGameID={GenerateGameID}
 				/>
 			</>
 		);
